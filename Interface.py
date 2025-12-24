e1f74d85ff6a57151eab93aeb4f41723ea131e7d
ClementSnt <clement.sonnet@protonmail.com>
2025-12-24 15:49:43 +0100

    Create Interface.py

---
+++ Interface.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.title("Accès")
+
+user_input = st.text_input("Entre le mot clé")
+
+if user_input:
+    if user_input == "Lolo":
+        st.success("Oui")
+    else:
+        st.error("Non")
