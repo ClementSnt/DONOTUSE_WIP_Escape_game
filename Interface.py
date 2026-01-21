6c6e91e09725b92b58558a8db8542adfc2ec534a
ClementSnt <clement.sonnet@protonmail.com>
2026-01-21 17:42:10 +0100

    Update Interface.py

--- Interface.py
+++ Interface.py
@@ -5,7 +5,7 @@ st.title("Accès")
 user_input = st.text_input("Entre le mot clé")
 
 if user_input:
-    if user_input == "Lolo":
+    if user_input == "test":
         st.success("Oui")
     else:
         st.error("Non")
